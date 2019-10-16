23b477066cbe96edc985374f65e6c88413527918
jojo <yoyosource@nidido.de>
2019-10-16 17:33:46 +0200

    Add ActionMove

---
+++ src/main/java/engine/uiBehavior/actions/ActionMove.java
@@ -0,0 +1,26 @@
+package engine.uiBehavior.actions;
+
+import engine.ui.Element;
+import engine.uiBehavior.Action;
+
+public class ActionMove extends Action {
+
+    public ActionMove() {
+        setId();
+    }
+
+    @Override
+    public void setElement(Element element) {
+        super.setElement(element);
+    }
+
+    @Override
+    public Element getElement() {
+        return super.getElement();
+    }
+
+    @Override
+    public void run() {
+        super.run();
+    }
+}
